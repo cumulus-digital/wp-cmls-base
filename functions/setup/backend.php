0aa413076ce39a8b549d37e20f59350d2d6503b8
Daniel Vena <deaconvena@gmail.com>
2020-09-01 14:26:27 -0400

    disable block directory

--- functions/setup/backend.php
+++ functions/setup/backend.php
@@ -8,6 +8,15 @@ if (!defined('ABSPATH')) die('No direct access allowed');
 // Disable theme and plugin editor
 define( 'DISALLOW_FILE_EDIT', true );
 
+// Disable the block directory
+\add_action(
+	'admin_init',
+	function() {
+		\remove_action( 'enqueue_block_editor_assets', 'wp_enqueue_editor_block_directory_assets' );
+		\remove_action( 'enqueue_block_editor_assets', 'gutenberg_enqueue_block_editor_assets_block_directory' );
+	}
+);
+
 function editorSetupStyles() {
 	\add_editor_style('build/backend.css');
 }
