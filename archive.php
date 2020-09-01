d0c2ecaead06abc892f10a5be550a5d7ba1f0f5d
Daniel Vena <deaconvena@gmail.com>
2020-09-01 17:29:38 -0400

    better way to preempt the loop for archives

--- archive.php
+++ archive.php
@@ -10,19 +10,26 @@ if (!defined('ABSPATH')) die('No direct access allowed');
 $currentQuery = \get_queried_object();
 $slug = '';
 if (property_exists($currentQuery, 'slug')) {
-    $slug = $currentQuery->slug;
+	$slug = $currentQuery->slug;
 } else if (property_exists($currentQuery, 'rewrite')) {
-    if (array_key_exists('slug', $currentQuery->rewrite)) {
-        $slug = $currentQuery->rewrite['slug'];
-    }
+	if (array_key_exists('slug', $currentQuery->rewrite)) {
+		$slug = $currentQuery->rewrite['slug'];
+	}
 }
 if ($slug) {
-    \query_posts('pagename=' . $slug);
-    if (\have_posts()) {
-        \get_template_part('singular', $post->post_type);
-        return;
-    }
-    \wp_reset_query();
+	$query = new \WP_Query(array(
+		'post_type' => 'page',
+		'name' => $slug
+	));
+	//\query_posts('pagename=' . $slug);
+	if ($query->have_posts()) {
+		$wp_query = $query;
+		$posts = $query->get_posts();
+		$post = array_shift($posts);
+		\get_template_part('singular', 'page');
+		return;
+	}
+	\wp_reset_query();
 }
 
 \get_header();
@@ -30,33 +37,33 @@ if ($slug) {
 
 <main role="main" class="archive">
 
-    <?php \get_template_part('templates/pages/archive-header'); ?>
+	<?php \get_template_part('templates/pages/archive-header'); ?>
 
-    <?php if (\have_posts()): ?>
+	<?php if (\have_posts()): ?>
 
-        <div class="row">
-            <div class="row-container cards">
-            <?php while (\have_posts()): \the_post() ?>
+		<div class="row">
+			<div class="row-container cards">
+			<?php while (\have_posts()): \the_post() ?>
 
-                <?php \get_template_part('templates/pages/base'); ?>
+				<?php \get_template_part('templates/pages/base'); ?>
 
-            <?php endwhile ?>
-            </div>
-        </div>
+			<?php endwhile ?>
+			</div>
+		</div>
 
-        <?php if (is_paginated()): ?>
-            <?php \get_template_part('templates/pages/pagination'); ?>
-        <?php endif ?>
+		<?php if (is_paginated()): ?>
+			<?php \get_template_part('templates/pages/pagination'); ?>
+		<?php endif ?>
 
-    <?php else: ?>
+	<?php else: ?>
 
-        <article class="row">
-            <div class="row-container">
-                <p>Sorry, nothing here.</p>
-            </div>
-        </article>
-    
-    <?php endif ?>
+		<article class="row">
+			<div class="row-container">
+				<p>Sorry, nothing here.</p>
+			</div>
+		</article>
+	
+	<?php endif ?>
 
 </main>
 
