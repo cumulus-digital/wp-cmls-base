fc2470a43798d1e4356ccdcff53f4560e35f4476
Daniel Vena <deaconvena+github@gmail.com>
2021-04-01 17:41:57 -0400

    Support descriptions for menu items

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '6791af1b189ccc7f1d70a594f7bfd3c1');
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '0ad16cf16a28a9a2022ac1ccf1ab95a6');
