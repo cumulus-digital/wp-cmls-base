e8e50e20606f1fccc084e7e1a95b09188ced5ed6
Daniel Vena <deaconvena+github@gmail.com>
2021-07-08 00:28:22 -0400

    minor composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,33 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ff2959c7d731f24c39ae7cea5f0229f89704f165',
+        'name' => '__root__',
+        'dev' => true,
     ),
-    'reference' => '7839f086b32ab77c980eb427cf4123076f07834b',
-    'name' => '__root__',
-  ),
-  'versions' => 
-  array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '7839f086b32ab77c980eb427cf4123076f07834b',
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ff2959c7d731f24c39ae7cea5f0229f89704f165',
+            'dev_requirement' => false,
+        ),
+        'wptrt/control-color-alpha' => array(
+            'pretty_version' => 'v1.1.4',
+            'version' => '1.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wptrt/control-color-alpha',
+            'aliases' => array(),
+            'reference' => 'c6ddf43b78430c8217c9822620aa40399a81d641',
+            'dev_requirement' => false,
+        ),
     ),
-    'wptrt/control-color-alpha' => 
-    array (
-      'pretty_version' => 'v1.1.4',
-      'version' => '1.1.4.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'c6ddf43b78430c8217c9822620aa40399a81d641',
-    ),
-  ),
 );
