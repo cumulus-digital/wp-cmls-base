7e2fbab10ff1fc1f10749551503071490ada938f
Daniel Vena <deaconvena+github@gmail.com>
2021-07-22 00:19:44 -0400

    additional cleanup hooks

--- functions/setup/cleanup.php
+++ functions/setup/cleanup.php
@@ -16,65 +16,65 @@ function remove_identity() {
 }
 \add_filter( 'the_generator', ns( 'remove_identity' ) );
 
-// Remove emoji crud
-\remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-\remove_action( 'wp_print_styles', 'print_emoji_styles' );
+\add_action( 'init', function () {
+	// Remove generator meta
+	\remove_action( 'wp_head', 'wp_generator' );
 
-// Remove MS-live writer
-\remove_action( 'wp_head', 'wlwmanifest_link' );
+	// Remove index rel link
+	\remove_action( 'wp_head', 'index_rel_link' );
 
-// Remove Really Simple Discovery
-\remove_action( 'wp_head', 'rsd_link' );
+	// Remove emoji crud
+	\remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	\remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	\remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	\remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	\remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	\remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 
-// Remove Comments Feed
-\remove_action( 'wp_head', 'feed_links_extra', 3 );
+	// Remove MS-live writer
+	\remove_action( 'wp_head', 'wlwmanifest_link' );
 
-// Remove Yoast annoyances
-if ( \defined( 'WPSEO_VERSION' ) ) {
-	// Remove Yoast ad
-	// https://buddydev.com/remove-this-site-is-optimized-with-the-yoast-seo-plugin-vx-y-z/
-	\add_action( 'template_redirect', function () {
-		if ( ! \class_exists( 'WPSEO_Frontend' ) ) {
-			return;
-		}
+	// Remove Really Simple Discovery
+	\remove_action( 'wp_head', 'rsd_link' );
 
-		$instance = WPSEO_Frontend::get_instance();
+	// Remove Comments Feed
+	\remove_action( 'wp_head', 'feed_links_extra', 3 );
 
-		// make sure, future version of the plugin does not break our site.
-		if ( ! \method_exists( $instance, 'debug_mark' ) ) {
-			return;
-		}
+	// Remove Yoast annoyances
+	if ( \defined( 'WPSEO_VERSION' ) ) {
+		// Remove Yoast ad
+		// https://buddydev.com/remove-this-site-is-optimized-with-the-yoast-seo-plugin-vx-y-z/
+		\add_action( 'template_redirect', function () {
+			if ( ! \class_exists( 'WPSEO_Frontend' ) ) {
+				return;
+			}
 
-		// ok, let us remove the love letter.
-		\remove_action( 'wpseo_head', [ $instance, 'debug_mark' ], 2 );
-	}, 9999 );
+			$instance = WPSEO_Frontend::get_instance();
 
-	// Move Yoast post box to bottom
-	\add_filter( 'wpseo_metabox_prio', function () {
-		return 'low';
-	} );
+			// make sure, future version of the plugin does not break our site.
+			if ( ! \method_exists( $instance, 'debug_mark' ) ) {
+				return;
+			}
 
-	// Remove Yoast comments
-	\add_filter( 'wpseo_debug_markers', '__return_false' );
+			// ok, let us remove the love letter.
+			\remove_action( 'wpseo_head', [ $instance, 'debug_mark' ], 2 );
+		}, 9999 );
 
-	// Remove Yoast admin notifications
-	\add_action( 'admin_init', function () {
-		if ( \class_exists( 'Yoast_Notification_Center' ) ) {
-			$yoast_nc = Yoast_Notification_Center::get();
-			\remove_action( 'admin_notices', [$yoast_nc, 'display_notifications'] );
-			\remove_action( 'all_admin_notices', [$yoast_nc, 'display_notifications'] );
-		}
-	} );
-}
+		// Move Yoast post box to bottom
+		\add_filter( 'wpseo_metabox_prio', function () {
+			return 'low';
+		} );
 
-// Remove users from automatically generated sitemap in WP 5.5
-\add_filter(
-	'wp_sitemaps_add_provider',
-	function ( $provider, $name = null ) {
-		if ( \is_a( $provider, 'WP_Sitemaps_Users' ) ) {
-			return false;
-		}
+		// Remove Yoast comments
+		\add_filter( 'wpseo_debug_markers', '__return_false' );
 
-		return $provider;
+		// Remove Yoast admin notifications
+		\add_action( 'admin_init', function () {
+			if ( \class_exists( 'Yoast_Notification_Center' ) ) {
+				$yoast_nc = Yoast_Notification_Center::get();
+				\remove_action( 'admin_notices', [$yoast_nc, 'display_notifications'] );
+				\remove_action( 'all_admin_notices', [$yoast_nc, 'display_notifications'] );
+			}
+		} );
 	}
-);
+} );
