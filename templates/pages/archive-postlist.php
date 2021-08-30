cbe2e47935056258756627c4589a6e659962c853
Daniel Vena <deaconvena+github@gmail.com>
2021-08-29 22:08:54 -0400

    archive-postlist allows overriding query

--- templates/pages/archive-postlist.php
+++ templates/pages/archive-postlist.php
@@ -7,14 +7,23 @@
 namespace CMLS_Base;
 
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
+
+if ( ! isset( $args['the_posts'] ) ) {
+	global $wp_query;
+	$args['the_posts'] = $wp_query;
+}
+
+if ( ! isset( $args['row-class'] ) ) {
+	$args['row-class'] = null;
+}
 ?>
 
-<div class="row">
+<div class="row <?php echo $args['row-class']; ?>">
 	<div class="row-container <?php echo $args['display_format']; ?>">
 
 	<?php \do_action( 'cmls_template-archive-before_posts' ); ?>
 
-	<?php while ( \have_posts() ): \the_post(); ?>
+	<?php while ( $args['the_posts']->have_posts() ): $args['the_posts']->the_post(); ?>
 
 		<?php
 			cmls_get_template_part( 'templates/pages/excerpt', make_post_class(), $args );
