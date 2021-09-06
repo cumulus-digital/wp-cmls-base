ff07f66a55720ea9a2045abfbc3f909e939bdc31
Daniel Vena <deaconvena+github@gmail.com>
2021-09-05 21:01:18 -0400

    better handling of default args

--- templates/pages/featured_image.php
+++ templates/pages/featured_image.php
@@ -8,12 +8,18 @@
 namespace CMLS_Base;
 
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
-$thumbnail_size = isset( $args['thumbnail_size'] ) ? $args['thumbnail_size'] : 'full';
-$force_image    = isset( $args['force_featured_image'] ) && $args['force_featured_image'] == true;
+$args = \array_merge(
+	[
+		'display_format'       => 'cards',
+		'thumbnail_size'       => 'full',
+		'force_featured_image' => false,
+	],
+	$args
+);
 ?>
 
 <?php // By default, only show featured image here in cat/tag lists?>
-<?php if ( ( ! \is_singular() || $force_image ) && \has_post_thumbnail() ): ?>
+<?php if ( ( ! \is_singular() || $args['force_featured_image'] ) && \has_post_thumbnail() ): ?>
 
 	<?php if ( $args['display_format'] !== 'cards' ): ?>
 		<a href="<?php \the_permalink(); ?>" title="<?php echo \esc_attr( \get_the_title() ); ?>" class="featured-image">
@@ -21,13 +27,13 @@ $force_image    = isset( $args['force_featured_image'] ) && $args['force_feature
 
 	<style>
 		.post-<?php \the_ID(); ?> {
-			--featured_image: url('<?php echo \esc_attr( \get_the_post_thumbnail_url( null, $thumbnail_size ) ); ?>');
+			--featured_image: url('<?php echo \esc_attr( \get_the_post_thumbnail_url( null, $args['thumbnail_size'] ) ); ?>');
 		}
 	</style>
 	<figure class="featured-image">
 
 		<?php
-			\the_post_thumbnail( $thumbnail_size, [ 'alt' => \esc_attr( \get_the_title() ) ] );
+			\the_post_thumbnail( $args['thumbnail_size'], [ 'alt' => \esc_attr( \get_the_title() ) ] );
 		?>
 
 	</figure>
