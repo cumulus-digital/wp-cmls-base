070834e4026409ced3eaa7364f5f694155350f0a
Daniel Vena <deaconvena+github@gmail.com>
2021-09-15 19:49:40 -0400

    better unique palette for acf color picker

--- functions/setup/acf/color-picker-palette.php
+++ functions/setup/acf/color-picker-palette.php
@@ -11,21 +11,36 @@ namespace CMLS_Base;
 \add_action( 'acf/input/admin_footer', function () {
 	$colors = themeMods::getColors();
 
+	// Convert theme colors to simple hex values
 	$colors = \array_map( function ( $color ) {
-		// Convert hex colors to rgba
-		if (
-			\is_string( $color )
-			&& \mb_substr( $color, 0, 1 ) === '#'
-			&& (
-				\mb_strlen( $color ) === 4 || \mb_strlen( $color ) === 7
-			)
-		) {
-			return 'rgba(' . rgbS( $color ) . ', 1)';
+		if ( \is_string( $color ) ) {
+			// Hex
+			if ( \mb_substr( $color, 0, 1 ) === '#' ) {
+				return \mb_strtoupper( $color );
+			}
+			// Rgba
+			if ( \mb_substr( $color, 0, 3 ) === 'rgb' ) {
+				$rgba = [];
+				$regex = '#\((([^()]+|(?R))*)\)#';
+
+				if ( \preg_match_all( $regex, $color, $matches ) ) {
+					$rgba = \explode( ',', \implode( ' ', $matches[1] ) );
+				} else {
+					$rgba = \explode( ',', $color );
+				}
+
+				$rr = \dechex( $rgba['0'] );
+				$gg = \dechex( $rgba['1'] );
+				$bb = \dechex( $rgba['2'] );
+
+				return \mb_strtoupper( "#$rr$gg$bb" );
+			}
 		}
 
 		return $color;
 	}, $colors );
 
+	// Remove duplicates
 	$colors = \array_filter( \array_unique( $colors, \SORT_STRING ) );
 
 	$colorString = "'" . \implode( "','", $colors ) . "'"; ?>
