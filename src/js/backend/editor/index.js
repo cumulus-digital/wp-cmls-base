961d3a9a57169ce8d7530c2dc07f926c615f3579
Daniel Vena <deaconvena+github@gmail.com>
2022-04-23 17:15:08 -0400

    start restructuring backend scripts

---
+++ src/js/backend/editor/index.js
@@ -0,0 +1,48 @@
+require('./acf-title.js');
+
+(() => {
+	// Only operate in the editor
+	if (!window?.wp?.blocks) {
+		return;
+	}
+
+	// Determine if we're in the main window or gutenberg's mobile iframe is active
+	function getContext(BOTH) {
+		const iframe = document.querySelector('iframe[name="editor-canvas"]');
+		const iframeContent =
+			iframe.contentDocument || iframe.contentWindow.document;
+		if (iframeContent && iframeCoontent.body) {
+			return iframeContent.bodu;
+		}
+		return document.body;
+	}
+
+	// Hide the sticky option, force it false
+	const styles = document.createElement('style');
+	styles.innerHTML = '#sticky-span { display: none !important; }';
+	document.body.appendChild(styles);
+	let isSticky = null;
+	const { select, subscribe } = wp.data;
+	const waitForEditor = subscribe(() => {
+		if (!select('core/editor').__unstableIsEditorReady()) {
+			return;
+		}
+		isSticky = select('core/editor').getEditedPostAttribute('sticky');
+		if (isSticky === undefined || isSticky === null) {
+			return;
+		}
+		const stickyControl = Array.from(
+			document.querySelectorAll(
+				'.edit-post-post-status .components-panel__row'
+			)
+		).find((el) => el.innerText === 'Stick to the top of the blog');
+		if (!stickyControl) {
+			return;
+		}
+		waitForEditor();
+		stickyControl.style.display = 'none';
+		if (isSticky) {
+			wp.data.dispatch('core/editor').editPost({ sticky: false });
+		}
+	});
+})();
---
+++ src/js/backend/editor/post-author.js
@@ -0,0 +1,63 @@
+/**
+ * Injects an alt display author field near the post author in gutenberg
+ */
+(() => {
+	// Only operate in the editor
+	if (!window?.wp?.blocks) {
+		return;
+	}
+
+	const { registerPlugin } = wp.plugins;
+	const { PluginPostStatusInfo } = wp.editPost;
+	const { TextControl } = wp.components;
+	const { useSelect, useDispatch } = wp.data;
+	const { useCallback } = wp.element;
+
+	const PluginPostStatusInfoTest = () => {
+		const { postType } = useSelect((select) => {
+			return { postType: select('core/editor').getCurrentPostType() };
+		});
+		if (postType === 'post') {
+			const { acfMeta } = useSelect((select) => {
+				return {
+					acfMeta:
+						select('core/editor').getEditedPostAttribute('acf'),
+				};
+			}, []);
+			const { editPost } = useDispatch('core/editor', [
+				acfMeta['cmls-alt_author'],
+			]);
+			const onChange = useCallback(
+				wp.compose.useDebounce((val) => {
+					// I think ACF's own metabox overwrites, so we'll do this there and in dispatch
+					const field = document.getElementById(
+						'acf-field_613a67efc94aa'
+					);
+					if (field) {
+						field.value = val;
+					}
+					editPost({ acf: { 'cmls-alt_author': val } });
+					wp.data.dispatch('core/editor').savePost();
+				}, 600),
+				[acfMeta['cmls-alt_author']]
+			);
+
+			return (
+				<PluginPostStatusInfo>
+					<TextControl
+						label="Alt. Display Author Name"
+						help="Set an alternate name for the author of this post, e.g. for guest blogs, without creating an account."
+						value={acfMeta['cmls-alt_author']}
+						onChange={onChange}
+					/>
+				</PluginPostStatusInfo>
+			);
+		} else {
+			return null;
+		}
+	};
+
+	registerPlugin('post-status-info-test', {
+		render: PluginPostStatusInfoTest,
+	});
+})();
