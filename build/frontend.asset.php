53888783543b1746613431b39d2756cafda5a29b
Daniel Vena <deaconvena+github@gmail.com>
2022-05-31 10:15:18 -0400

    remove side padding for page titles on mobile

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'bce29e2a69e13ad59a02');
+<?php return array('dependencies' => array('jquery'), 'version' => 'e2b975a5edd14f9be3fe');
