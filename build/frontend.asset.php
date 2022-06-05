ce81a41bafed7cb35dc1d60554b372f89293824f
Daniel Vena <deaconvena+github@gmail.com>
2022-06-05 13:11:04 -0400

    fix scroll margin support for additions

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '98c6bba9b1c95f5d21c7');
+<?php return array('dependencies' => array('jquery'), 'version' => '012b3b31c4ecfeaa1fd1');
