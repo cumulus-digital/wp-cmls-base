19012ab5dec469c9cd6953283c1e8ceeae1d4c6e
Daniel Vena <deaconvena+github@gmail.com>
2023-01-03 16:22:55 -0500

    package upgrades

--- build/backend.asset.php
+++ build/backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'wp-element', 'wp-polyfill'), 'version' => 'e37aad1738dda8770a8f');
+<?php return array('dependencies' => array('jquery', 'lodash', 'wp-element', 'wp-polyfill'), 'version' => 'e499c9007bf21947f707');
--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'bd5168609864a750280e');
+<?php return array('dependencies' => array('jquery'), 'version' => '4ded074252e20acd6ac1');
