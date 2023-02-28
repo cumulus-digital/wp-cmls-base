e26f3466dfe84becc19a1424a662bfd25e0498fa
Daniel Vena <deaconvena+github@gmail.com>
2023-02-28 11:56:28 -0500

    package update, remove underline from button links

--- build/backend.asset.php
+++ build/backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'wp-element', 'wp-polyfill'), 'version' => 'e499c9007bf21947f707');
+<?php return array('dependencies' => array('jquery', 'lodash', 'wp-element', 'wp-polyfill'), 'version' => 'cb759ae6697124df5f55');
--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '4ded074252e20acd6ac1');
+<?php return array('dependencies' => array('jquery'), 'version' => '46b6e3dd7f50be6c0ebc');
