9a1900fd01d1ad411fef899c663499afa507cbbe
Daniel Vena <deaconvena+github@gmail.com>
2023-04-14 18:13:15 -0400

    redesigning alt author field to be more responsive

--- build/backend.asset.php
+++ build/backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'wp-element', 'wp-polyfill'), 'version' => 'c1caab56926ddbc0d7bf');
+<?php return array('dependencies' => array('jquery', 'lodash', 'wp-element', 'wp-polyfill'), 'version' => 'b3e69cacb636d72c50d0');
--- src/js/backend/editor/post-author.js
+++ src/js/backend/editor/post-author.js
@@ -1,9 +1,9 @@
 /**
  * Injects an alt display author field near the post author in gutenberg
  */
-(() => {
+( () => {
 	// Only operate in the editor
-	if (!window?.wp?.blocks) {
+	if ( ! window?.wp?.blocks ) {
 		return;
 	}
 
@@ -11,53 +11,100 @@
 	const { PluginPostStatusInfo } = wp.editPost;
 	const { TextControl } = wp.components;
 	const { useSelect, useDispatch } = wp.data;
-	const { useCallback } = wp.element;
+	const { useCallback, useState } = wp.element;
+	const { debounce } = wp.compose;
 
-	const PluginPostStatusInfoTest = () => {
-		const { postType } = useSelect((select) => {
-			return { postType: select('core/editor').getCurrentPostType() };
-		});
-		if (postType === 'post') {
-			const { acfMeta } = useSelect((select) => {
-				return {
-					acfMeta:
-						select('core/editor').getEditedPostAttribute('acf'),
-				};
-			}, []);
-			const { editPost } = useDispatch('core/editor', [
-				acfMeta['cmls-alt_author'],
-			]);
-			const onChange = useCallback(
-				wp.compose.useDebounce((val) => {
-					// I think ACF's own metabox overwrites, so we'll do this there and in dispatch
-					const field = document.getElementById(
-						'acf-field_613a67efc94aa'
-					);
-					if (field) {
-						field.value = val;
-					}
-					editPost({ acf: { 'cmls-alt_author': val } });
-					wp.data.dispatch('core/editor').savePost();
-				}, 600),
-				[acfMeta['cmls-alt_author']]
-			);
+	const acfFieldId = 'field_613a67efc94aa';
 
-			return (
-				<PluginPostStatusInfo>
-					<TextControl
-						label="Alt. Author Display Name"
-						help="Set an alternate name for the author of this post to display publicaly instead of the real author, e.g. for guest blogs."
-						value={acfMeta['cmls-alt_author']}
-						onChange={onChange}
-					/>
-				</PluginPostStatusInfo>
-			);
-		} else {
+	const altAuthorField = () => {
+		const { postType } = useSelect( ( select ) => {
+			return { postType: select( 'core/editor' ).getCurrentPostType() };
+		} );
+
+		// Only operate on posts
+		if ( postType !== 'post' ) {
 			return null;
 		}
+
+		// Fetch the initial value from ACF data
+		const [ altAuthor, setAltAuthor ] = useState(
+			useSelect( ( select ) => {
+				const acfMeta =
+					select( 'core/editor' ).getEditedPostAttribute( 'acf' );
+				return acfMeta[ 'cmls-alt_author' ];
+			} )
+		);
+		const [ updatingACF, setUpdatingACF ] = useState( false );
+
+		const { editPost } = useDispatch( 'core/editor', [ altAuthor ] );
+
+		// Update the ACF field and trigger an editPost
+		// so the editor knows we've changed data
+		const updateAltAuthor = debounce(
+			( val ) => {
+				setAltAuthor( val );
+				if ( window.acf ) {
+					const field = window.acf.getField( acfFieldId );
+					if ( field.val() !== val ) {
+						setUpdatingACF( true );
+						field.val( val );
+					}
+				}
+				setUpdatingACF( false );
+				editPost( { acf: { 'cmls-alt_author': val } } );
+			},
+			150,
+			{ trailing: true }
+		);
+
+		const onChange = useCallback( updateAltAuthor, [ altAuthor ] );
+
+		// Watch the original ACF field for changes and reflect it in our state
+		if ( window.acf ) {
+			const acf = window.acf;
+			const acfField = acf.getField( acfFieldId );
+			if ( acfField && ! acfField.get( 'watching_alt_author' ) ) {
+				[ 'change', 'keyup' ].forEach( ( ev ) => {
+					acfField.on(
+						ev,
+						debounce(
+							() => {
+								if (
+									updatingACF ||
+									acfField.val() === altAuthor
+								) {
+									return;
+								}
+								updateAltAuthor( acfField.val() );
+							},
+							150,
+							{ trailing: true }
+						)
+					);
+				} );
+				acfField.set( 'watching_alt_author', true );
+			}
+		}
+
+		return (
+			<PluginPostStatusInfo>
+				<TextControl
+					label="Alt. Author Display Name"
+					help="Set an alternate name for the author of this post to display publicaly instead of the real author, e.g. for guest blogs."
+					value={ altAuthor }
+					onChange={ onChange }
+				/>
+			</PluginPostStatusInfo>
+		);
 	};
 
-	registerPlugin('post-status-info-test', {
-		render: PluginPostStatusInfoTest,
-	});
-})();
+	if ( window.acf && window.acf.getField( acfFieldId ) ) {
+		const field = window.acf.getField( acfFieldId );
+		if ( ! field.get( 'alt_author_registered' ) ) {
+			registerPlugin( 'post-status-info-test', {
+				render: altAuthorField,
+			} );
+			field.set( 'alt_author_registered', true );
+		}
+	}
+} )();
