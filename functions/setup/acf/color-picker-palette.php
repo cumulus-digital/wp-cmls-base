7dfa1eb960c0658bfd9c5ce16e5f1574d0549085
Daniel Vena <deaconvena+github@gmail.com>
2023-07-04 14:23:56 -0400

    use wp_add_inline_script

--- functions/setup/acf/color-picker-palette.php
+++ functions/setup/acf/color-picker-palette.php
@@ -8,7 +8,7 @@ namespace CMLS_Base;
 
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
 
-\add_action( 'acf/input/admin_footer', function () {
+\add_action( 'admin_enqueue_scripts', function () {
 	$colors = themeMods::getColors();
 
 	// Convert theme colors to simple hex values
@@ -20,7 +20,7 @@ namespace CMLS_Base;
 			}
 			// Rgba
 			if ( \mb_substr( $color, 0, 3 ) === 'rgb' ) {
-				$rgba = [];
+				$rgba  = [];
 				$regex = '#\((([^()]+|(?R))*)\)#';
 
 				if ( \preg_match_all( $regex, $color, $matches ) ) {
@@ -43,15 +43,24 @@ namespace CMLS_Base;
 	// Remove duplicates
 	$colors = \array_filter( \array_unique( $colors, \SORT_STRING ) );
 
-	$colorString = "'" . \implode( "','", $colors ) . "'"; ?>
-	<script>
-		acf.add_filter('color_picker_args', function( args, field ){
+	$colorString = "'" . \implode( "','", $colors ) . "'";
 
+	\do_action( 'qm/debug', ['adding colors2', $colorString] );
+
+	\wp_register_script( PREFIX . '-acf_colors', '', [], false, true );
+	\wp_enqueue_script( PREFIX . '-acf_colors' );
+	\wp_add_inline_script(
+		PREFIX . '-acf_colors',
+		"
+		if (window.acf) {
 			// Add customizer colors to ACF color picker
-			args.palettes = [<?php echo $colorString; ?>];
-			return args;
-
-		});
-	</script>
-	<?php
+			console.log('Adding customizer colors to ACF color picker');
+			acf.add_filter('color_picker_args', function(args, field) {
+				console.log('Added them!');
+				args.palettes = [{$colorString}];
+				return args;
+			});
+		}
+		"
+	);
 } );
