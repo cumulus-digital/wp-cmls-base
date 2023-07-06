67f81b9c41c7384b61cce7499187e37ceeda0e7d
Daniel Vena <deaconvena+github@gmail.com>
2023-07-06 14:08:00 -0400

    filter more unused jetpack stuff

--- functions/setup/plugin_hacks/jetpack.php
+++ functions/setup/plugin_hacks/jetpack.php
@@ -1,40 +1,46 @@
 <?php
 /**
- * JC Submenu.
+ * Remove Jetpack Crud.
  */
 
 namespace CMLS_Base\Setup\PluginHacks;
 
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
 
-\add_filter( 'jetpack_sharing_counts', '__return_false', 99 );
-\add_filter( 'jetpack_implode_frontend_css', '__return_false', 99 );
+\add_filter( 'jetpack_sharing_counts', '__return_false', \PHP_INT_MAX );
+\add_filter( 'jetpack_implode_frontend_css', '__return_false', \PHP_INT_MAX );
 
 function remove_jetpack_css() {
-	\wp_deregister_style( 'AtD_style' ); // After the Deadline
-	\wp_deregister_style( 'jetpack_likes' ); // Likes
-	\wp_deregister_style( 'jetpack_related-posts' ); // Related Posts
+	$jetpack_options = \get_option( 'jetpack_active_modules' );
+	if ( $jetpack_options ) {
+		if ( ! \in_array( 'videopress', $jetpack_options ) ) {
+			\wp_deregister_style( 'jetpack-videopress-video-block-view' ); // VideoPress Video Block
+		}
+	}
+	// \wp_deregister_style( 'AtD_style' ); // After the Deadline
+	// \wp_deregister_style( 'jetpack_likes' ); // Likes
+	// \wp_deregister_style( 'jetpack_related-posts' ); // Related Posts
 	// \wp_deregister_style( 'jetpack-carousel' ); // Carousel
 	// \wp_deregister_style( 'grunion.css' ); // Grunion contact form
-	\wp_deregister_style( 'the-neverending-homepage' ); // Infinite Scroll
-	\wp_deregister_style( 'infinity-twentyten' ); // Infinite Scroll - Twentyten Theme
-	\wp_deregister_style( 'infinity-twentyeleven' ); // Infinite Scroll - Twentyeleven Theme
-	\wp_deregister_style( 'infinity-twentytwelve' ); // Infinite Scroll - Twentytwelve Theme
-	\wp_deregister_style( 'noticons' ); // Notes
-	\wp_deregister_style( 'post-by-email' ); // Post by Email
-	\wp_deregister_style( 'publicize' ); // Publicize
-	\wp_deregister_style( 'sharedaddy' ); // Sharedaddy
-	\wp_deregister_style( 'sharing' ); // Sharedaddy Sharing
+	// \wp_deregister_style( 'the-neverending-homepage' ); // Infinite Scroll
+	// \wp_deregister_style( 'infinity-twentyten' ); // Infinite Scroll - Twentyten Theme
+	// \wp_deregister_style( 'infinity-twentyeleven' ); // Infinite Scroll - Twentyeleven Theme
+	// \wp_deregister_style( 'infinity-twentytwelve' ); // Infinite Scroll - Twentytwelve Theme
+	// \wp_deregister_style( 'noticons' ); // Notes
+	// \wp_deregister_style( 'post-by-email' ); // Post by Email
+	// \wp_deregister_style( 'publicize' ); // Publicize
+	// \wp_deregister_style( 'sharedaddy' ); // Sharedaddy
+	// \wp_deregister_style( 'sharing' ); // Sharedaddy Sharing
 	// \wp_deregister_style( 'stats_reports_css' ); // Stats
 	// \wp_deregister_style( 'jetpack-widgets' ); // Widgets
 	// \wp_deregister_style( 'jetpack-slideshow' ); // Slideshows
 	// \wp_deregister_style( 'presentations' ); // Presentation shortcode
 	// \wp_deregister_style( 'jetpack-subscriptions' ); // Subscriptions
-	\wp_deregister_style( 'tiled-gallery' ); // Tiled Galleries
-	\wp_deregister_style( 'widget-conditions' ); // Widget Visibility
-	\wp_deregister_style( 'jetpack_display_posts_widget' ); // Display Posts Widget
+	// \wp_deregister_style( 'tiled-gallery' ); // Tiled Galleries
+	// \wp_deregister_style( 'widget-conditions' ); // Widget Visibility
+	// \wp_deregister_style( 'jetpack_display_posts_widget' ); // Display Posts Widget
 	// \wp_deregister_style( 'gravatar-profile-widget' ); // Gravatar Widget
-	\wp_deregister_style( 'widget-grid-and-list' ); // Top Posts widget
-	\wp_deregister_style( 'jetpack-widgets' ); // Widgets
+	// \wp_deregister_style( 'widget-grid-and-list' ); // Top Posts widget
+	// \wp_deregister_style( 'jetpack-widgets' ); // Widgets
 }
-// \add_action( 'wp_print_styles', __NAMESPACE__ . '\\remove_jetpack_css' );
+\add_action( 'wp_print_styles', __NAMESPACE__ . '\\remove_jetpack_css' );
