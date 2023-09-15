d55de5ba7f7fb7f921981447db742352898b919e
Daniel Vena <deaconvena+github@gmail.com>
2023-09-15 11:00:48 -0400

    disable async style loader for now, conflict on CPN

--- functions/setup/filters/index.php
+++ functions/setup/filters/index.php
@@ -8,7 +8,7 @@ namespace CMLS_Base;
 
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
 
-require __DIR__ . '/display/style-loader-tag.php';
+// require __DIR__ . '/display/style-loader-tag.php';
 // require __DIR__ . '/display/async-jquery.php';
 require __DIR__ . '/display/index.php';
 require __DIR__ . '/database/index.php';
