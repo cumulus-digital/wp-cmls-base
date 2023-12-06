dae5f26c5063b5aa101e9026f2bd6683c3fa9cdc
Daniel Vena <deaconvena+github@gmail.com>
2023-12-06 16:46:36 -0500

    remove AIOSEO admin pages, add caching

--- functions/setup/plugin_hacks/aioseo.php
+++ functions/setup/plugin_hacks/aioseo.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Hacks for All In One SEO
+ * Hacks for All In One SEO.
  */
 
 namespace CMLS_Base\Setup\PluginHacks;
@@ -9,7 +9,9 @@ use CMLS_Base\CMLS_Cache;
 
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
 
-if ( \function_exists( 'aioseo' ) ) {
+function handleAioSeo() {
+	$cache_group       = 'CumulusTheme::aioseoPostsNoindex';
+	$cache_key_noindex = 'aioseo_posts_noindex';
 
 	// Collapse the AIOSEO editor metabox by default
 	\add_action( 'current_screen', function () {
@@ -33,19 +35,21 @@ if ( \function_exists( 'aioseo' ) ) {
 	\add_action( 'admin_menu', function () {
 		// Redirects under Tools
 		\remove_submenu_page( 'tools.php', \admin_url( '/admin.php?page=aioseo-redirects' ) );
+		\remove_submenu_page( 'aioseo', 'aioseo-link-assistant' );
+		\remove_submenu_page( 'aioseo', 'aioseo-redirects' );
+		\remove_submenu_page( 'aioseo', 'aioseo-local-seo' );
+		\remove_submenu_page( 'aioseo', 'aioseo-seo-analysis' );
+		\remove_submenu_page( 'aioseo', 'aioseo-search-statistics' );
 	}, \PHP_INT_MAX );
 
 	// Exclude specifically noindexed content from site search results
-	\add_filter( 'pre_get_posts', function ( $query ) {
+	\add_filter( 'pre_get_posts', function ( $query ) use ( $cache_group, $cache_key_noindex ) {
 		if ( \is_admin() ) {
 			return $query;
 		}
 
 		if ( \function_exists( 'aioseo' ) && $query->is_main_query() && $query->is_search ) {
-			$cache_key   = 'aioseo_posts_noindex';
-			$cache_group = 'CMLS_Base::aioseoPostsNoindex';
-
-			$cached = CMLS_Cache::get( $cache_key, $cache_group );
+			$cached = CMLS_Cache::get( $cache_key_noindex, $cache_group );
 
 			if ( $cached !== false ) {
 				$noIndex = $cached;
@@ -54,7 +58,7 @@ if ( \function_exists( 'aioseo' ) ) {
 				$noIndex = $wpdb->get_col( "
 					SELECT post_id FROM {$wpdb->prefix}aioseo_posts WHERE robots_noindex = 1
 				" );
-				CMLS_Cache::set( $cache_key, $noIndex, $cache_group );
+				CMLS_Cache::set( $cache_key_noindex, $noIndex, $cache_group );
 			}
 
 			if ( $noIndex && \count( $noIndex ) ) {
@@ -67,7 +71,11 @@ if ( \function_exists( 'aioseo' ) ) {
 		return $query;
 	} );
 	// Clear the noindex cache on post save
-	\add_filter( 'post_save', function () {
-		CMLS_Cache::delete( 'aioseo_posts_noindex', 'CMLS_Base::aioseoPostsNoindex' );
+	\add_filter( 'post_save', function () use ( $cache_group, $cache_key_noindex ) {
+		CMLS_Cache::delete( $cache_key_noindex, $cache_group );
 	} );
+}
+
+if ( \function_exists( 'aioseo' ) ) {
+	handleAioSeo();
 }
