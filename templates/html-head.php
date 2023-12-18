d471c53d83cd120f8efa3fdd32cc27914c60d247
Daniel Vena <deaconvena+github@gmail.com>
2023-12-17 21:25:56 -0500

    add action slots for head begin and end

--- templates/html-head.php
+++ templates/html-head.php
@@ -2,7 +2,7 @@
 /**
  * CMLS Base Theme
  * Template
- * HTML Head
+ * HTML Head.
  */
 
 namespace CMLS_Base;
@@ -12,19 +12,25 @@ namespace CMLS_Base;
 ?><!doctype html>
 <html <?php \language_attributes(); ?> class="no-js">
 <head>
+	<?php if ( \has_action( 'cmls_template-head-begin' ) ): ?>
+		<!-- action:cmls_template-head-begin -->
+		<?php \do_action( 'cmls_template-head-begin' ); ?>
+		<!-- /action:cmls_template-head-begin -->
+	<?php endif; ?>
+
 	<meta charset="<?php \bloginfo( 'charset' ); ?>">
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, shrink-to-fit=no">
 	<meta name="description" content="<?php
 		if ( \is_singular() && \has_excerpt() ) {
-			echo \wp_kses( \get_the_excerpt(), [] );
+			echo \wp_kses( \get_the_excerpt(), array() );
 		} elseif ( \get_bloginfo( 'description' ) ) {
-			echo \wp_kses( \get_bloginfo( 'description' ), [] );
+			echo \wp_kses( \get_bloginfo( 'description' ), array() );
 		} else {
-			echo \wp_kses( \get_bloginfo( 'name' ), [] );
+			echo \wp_kses( \get_bloginfo( 'name' ), array() );
 		}
-	?>">
+?>">
 
 	<?php \wp_site_icon(); ?>
 
@@ -42,4 +48,10 @@ namespace CMLS_Base;
 
 	<?php cmls_get_template_part( 'templates/child-html-head' ); ?>
 	<?php \wp_head(); ?>
+
+	<?php if ( \has_action( 'cmls_template-head-end' ) ): ?>
+		<!-- action:cmls_template-head-end -->
+		<?php \do_action( 'cmls_template-head-end' ); ?>
+		<!-- /action:cmls_template-head-end -->
+	<?php endif; ?>
 </head>
