a41173880083a895ffad443bb395a76d4f0df7d1
Daniel Vena <deaconvena+github@gmail.com>
2024-11-06 11:27:50 -0500

    add category classes to body tag on singular posts

--- header.php
+++ header.php
@@ -15,15 +15,45 @@ if ( \is_singular() ) {
 
 cmls_get_template_part( 'templates/html-head' );
 
-// Add a slug class to body classes based on post_name
-$slug = null;
-$qo   = \get_queried_object();
+// Gather additional body classes
+$css_classes = array();
+$qo          = \get_queried_object();
 
-if ( \is_object( $qo ) && \property_exists( $qo, 'post_name' ) ) {
-	$slug = 'slug-' . \esc_attr( $qo->post_name );
+if ( \is_object( $qo ) ) {
+	if ( \property_exists( $qo, 'post_name' ) ) {
+		$css_classes[] = 'slug-' . \sanitize_html_class( $qo->post_name );
+	}
+	// Excerpted from get_post_class, adds taxonomy classes to body class list
+	if ( \is_singular() ) {
+		$taxonomies = \get_taxonomies( array( 'public' => true ) );
+		$taxonomies = \apply_filters( 'post_class_taxonomies', $taxonomies, $qo->ID, $css_classes, '' );
+		foreach ( (array) $taxonomies as $taxonomy ) {
+			if ( \is_object_in_taxonomy( $qo->post_type, $taxonomy ) ) {
+				foreach ( (array) \get_the_terms( $qo->ID, $taxonomy ) as $term ) {
+					if ( empty( $term->slug ) ) {
+						continue;
+					}
+
+					$term_class = \sanitize_html_class( $term->slug, $term->term_id );
+					if ( \is_numeric( $term_class ) || ! \trim( $term_class, '-' ) ) {
+						$term_class = $term->term_id;
+					}
+
+					// 'post_tag' uses the 'tag' prefix for backward compatibility.
+					if ( 'post_tag' === $taxonomy ) {
+						$css_classes[] = 'tag-' . $term_class;
+					} else {
+						$css_classes[] = \sanitize_html_class( $taxonomy . '-' . $term_class, $taxonomy . '-' . $term->term_id );
+					}
+				}
+			}
+		}
+	}
+	$css_classes = \array_map( 'esc_attr', $css_classes );
 }
+
 ?>
-<body <?php \body_class( $slug ); ?>>
+<body <?php \body_class( $css_classes ); ?>>
 
 <?php
 	if ( \function_exists( 'wp_body_open' ) ) {
