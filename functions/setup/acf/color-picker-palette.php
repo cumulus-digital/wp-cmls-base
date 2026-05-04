f5e3f7a14ae08b32fc832ed801dc430eb97ff641
Daniel Vena <deaconvena+github@gmail.com>
2026-05-04 09:06:24 -0400

    better color palette normalizing

--- functions/setup/acf/color-picker-palette.php
+++ functions/setup/acf/color-picker-palette.php
@@ -13,37 +13,41 @@ namespace CMLS_Base;
 
 	// Convert theme colors to simple hex values
 	$colors = \array_map( function ( $color ) {
-		if ( \is_string( $color ) ) {
-			// Hex
-			if ( \mb_substr( $color, 0, 1 ) === '#' ) {
-				return \mb_strtoupper( $color );
+		if ( ! \is_string( $color ) ) {
+			return $color;
+		}
+
+		// Hex color normalization
+		if ( \mb_substr( $color, 0, 1 ) === '#' ) {
+			$hex = \sanitize_hex_color( $color );
+			return $hex ? \mb_strtoupper( $hex ) : $color;
+		}
+
+		// RGB/RGBA normalization
+		if ( \mb_substr( $color, 0, 3 ) === 'rgb' ) {
+			$rgba  = array();
+			$regex = '#\((([^()]+|(?R))*)\)#';
+
+			if ( \preg_match_all( $regex, $color, $matches ) ) {
+				$rgba = \explode( ',', \implode( ' ', $matches[1] ) );
+			} else {
+				$rgba = \explode( ',', $color );
 			}
-			// Rgba
-			if ( \mb_substr( $color, 0, 3 ) === 'rgb' ) {
-				$rgba  = array();
-				$regex = '#\((([^()]+|(?R))*)\)#';
 
-				if ( \preg_match_all( $regex, $color, $matches ) ) {
-					$rgba = \explode( ',', \implode( ' ', $matches[1] ) );
-				} else {
-					$rgba = \explode( ',', $color );
-				}
+			$rr = \dechex( $rgba['0'] );
+			$gg = \dechex( $rgba['1'] );
+			$bb = \dechex( $rgba['2'] );
 
-				$rr = \dechex( $rgba['0'] );
-				$gg = \dechex( $rgba['1'] );
-				$bb = \dechex( $rgba['2'] );
-
-				return \mb_strtoupper( "#{$rr}{$gg}{$bb}" );
-			}
+			return \mb_strtoupper( "#{$rr}{$gg}{$bb}" );
 		}
 
 		return $color;
 	}, $colors );
 
 	// Remove duplicates
-	$colors = \array_filter( \array_unique( $colors, \SORT_STRING ) );
+	$colors = \array_values( \array_filter( \array_unique( $colors, \SORT_STRING ) ) );
 
-	$colorString = "'" . \implode( "','", $colors ) . "'";
+	$color_json = json_encode( $colors );
 
 	\wp_register_script( PREFIX . '-acf_colors', '', array(), false, true );
 	\wp_enqueue_script( PREFIX . '-acf_colors' );
@@ -53,7 +57,7 @@ namespace CMLS_Base;
 		if (window.acf) {
 			// Add customizer colors to ACF color picker
 			acf.add_filter('color_picker_args', function(args, field) {
-				args.palettes = [{$colorString}];
+				args.palettes = {$color_json};
 				return args;
 			});
 		}
