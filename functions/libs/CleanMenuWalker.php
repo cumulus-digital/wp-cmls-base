005949af951fe1879e6336190db974a6696047d3
Daniel Vena <deaconvena+github@gmail.com>
2026-05-06 14:18:08 -0400

    php-cs-fixer

--- functions/libs/CleanMenuWalker.php
+++ functions/libs/CleanMenuWalker.php
@@ -1,39 +1,38 @@
 <?php
+
 /**
- * Custom menu walker to clean up menu displays
+ * Custom menu walker to clean up menu displays.
  */
 
 namespace CMLS_Base;
 
-use Walker_Nav_Menu;
-
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
 
-class CleanMenuWalker extends Walker_Nav_Menu {
-
+class CleanMenuWalker extends \Walker_Nav_Menu {
 	/**
-	 * Traverse elements to create list from elements.
-	 *
-	 * This override efficiently filters out items referencing non-published posts
-	 * using a single lightweight ID-only query to minimize memory overhead.
-	 *
-	 * @param array $elements  List of elements.
-	 * @param int   $max_depth Max depth to traverse.
-	 * @param mixed ...$args   Additional arguments.
-	 * @return string Resulting HTML.
-	 */
+		 * Traverse elements to create list from elements.
+		 *
+		 * This override efficiently filters out items referencing non-published posts
+		 * using a single lightweight ID-only query to minimize memory overhead.
+		 *
+		 * @param array $elements  list of elements
+		 * @param int   $max_depth max depth to traverse
+		 * @param mixed ...$args   Additional arguments.
+		 *
+		 * @return string resulting HTML
+		 */
 	public function walk( $elements, $max_depth, ...$args ) {
-		$post_ids = [];
+		$post_ids = array();
 		foreach ( $elements as $item ) {
 			if ( 'post_type' === $item->type ) {
 				$post_ids[] = (int) $item->object_id;
 			}
 		}
 
-		$published_ids = [];
+		$published_ids = array();
 		if ( ! empty( $post_ids ) ) {
 			// Fetch ONLY the IDs of posts that are actually published.
-			$published_ids = \get_posts( [
+			$published_ids = \get_posts( array(
 				'post__in'               => $post_ids,
 				'post_type'              => 'any',
 				'post_status'            => 'publish',
@@ -42,7 +41,7 @@ class CleanMenuWalker extends Walker_Nav_Menu {
 				'no_found_rows'          => true,
 				'update_post_meta_cache' => false,
 				'update_post_term_cache' => false,
-			] );
+			) );
 
 			// Ensure we have an array of integers for reliable comparison.
 			$published_ids = \array_map( 'intval', $published_ids );
@@ -71,7 +70,7 @@ class CleanMenuWalker extends Walker_Nav_Menu {
 		$indent = \str_repeat( $t, $depth );
 
 		// Default class.
-		$classes = [ 'sub-menu' ];
+		$classes = array( 'sub-menu' );
 
 		/**
 		 * Filters the CSS class(es) applied to a menu list element.
@@ -85,21 +84,21 @@ class CleanMenuWalker extends Walker_Nav_Menu {
 		$class_names = \implode( ' ', \apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
 		$class_names = $class_names ? ' class="' . \esc_attr( $class_names ) . '"' : '';
 
-		$output .= "{$n}{$indent}<ul$class_names role='menu'>{$n}";
+		$output .= "{$n}{$indent}<ul{$class_names} role='menu'>{$n}";
 	}
 
-	public function start_el( &$output, $item, $depth = 0, $args = [], $id = 0 ) {
+	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 		$indent = ( $depth ) ? \str_repeat( "\t", $depth ) : '';
 
-		$classes = empty( $item->classes ) ? [] : (array) $item->classes;
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 
 		$class_names = \join(
 			' ',
 			\apply_filters(
 				'nav_menu_css_class',
 				\array_filter( $classes ),
-				$item
-			)
+				$item,
+			),
 		);
 		$class_names = \esc_attr( $class_names );
 
@@ -109,10 +108,10 @@ class CleanMenuWalker extends Walker_Nav_Menu {
 
 		$output .= "{$indent}<li itemprop='name' id='menu-item-{$item->ID}' role='{$role}' class='{$class_names}'>";
 
-		$attributes = [
+		$attributes = array(
 			'itemprop' => ! empty( $item->itemprop ) ? \esc_html( $item->itemprop ) : 'url',
 			'tabindex' => ! empty( $item->tabindex ) ? \esc_html( $item->tabindex ) : '0',
-		];
+		);
 
 		if ( ! empty( $item->url ) ) {
 			$attributes['href'] = $item->url;
@@ -154,7 +153,7 @@ class CleanMenuWalker extends Walker_Nav_Menu {
 		$item_output .= "{$args->link_before}{$filtered_title}{$args->link_after}";
 
 		if ( ! empty( $item->description ) && $args->show_description ) {
-			$desc = \wp_kses( $item->description, ['em', 'i', 'strong', 'b', 'small', 'u'] );
+			$desc = \wp_kses( $item->description, array( 'em', 'i', 'strong', 'b', 'small', 'u' ) );
 			$item_output .= "<small>{$desc}</small>";
 		}
 
